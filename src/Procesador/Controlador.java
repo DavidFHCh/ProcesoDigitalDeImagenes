2cf5c6bffc1e291c2f5fc697a436ae5ff3a63780
DavidFHCh <davfep_96@hotmail.com>
2016-08-11 21:58:14 -0500

    Ya se pueden abrir imagenes.

--- src/Procesador/Controlador.java
+++ src/Procesador/Controlador.java
@@ -1,4 +1,4 @@
-package Procesador
+package Procesador;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -6,13 +6,43 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
-
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import java.io.*;
+import javafx.stage.FileChooser;
+import javafx.stage.FileChooser.ExtensionFilter;
 /**
  *
  * @author davif
  */
 public class Controlador implements Initializable {
     
+    @FXML
+    private ImageView imagenOriginal;
+
+
+    @FXML
+    private ImageView imagenProcesada;
+    
+    
+    @FXML
+    private void handleButtonAction(ActionEvent event) {
+        //algo
+    }
+
+    @FXML
+    private void abrirImagen(ActionEvent abrirImg) throws FileNotFoundException {
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle("Seleccionar Imagen");
+        fileChooser.getExtensionFilters().addAll(
+                new ExtensionFilter("Image Files", "*.png", "*.jpg"));
+        File selectedFile = fileChooser.showOpenDialog(null);
+        if (selectedFile != null) {
+            Image image = new Image(new FileInputStream(selectedFile));
+            imagenOriginal.setImage(image);
+            imagenProcesada.setImage(image);
+        }
+    }
     
     @Override
     public void initialize(URL url, ResourceBundle rb) {
