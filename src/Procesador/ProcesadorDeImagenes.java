efe65c23af4ca2eb088a2209960cd427ac78688d
DavidFHCh <davfep_96@hotmail.com>
2016-08-11 20:08:40 -0500

    Ya se tiene una interfaz grafica sencilla, todavia no hace nada.

---
+++ src/Procesador/ProcesadorDeImagenes.java
@@ -0,0 +1,34 @@
+package Procesador;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+/**
+ * Clase Principal.
+ * @author davif
+ */
+public class ProcesadorDeImagenes extends Application {
+    
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("procesoDigital.fxml"));
+        
+        Scene scene = new Scene(root);
+        
+        stage.setTitle("The Eye");
+        stage.setScene(scene);
+        stage.setResizable(false);
+        stage.show();
+    }
+
+    /**
+     * @param args de la la linea de comandos.
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+}
