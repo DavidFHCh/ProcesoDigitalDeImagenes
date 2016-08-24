85448580d5165e29c9c20ce1f6a4321aeffdf6bd
DavidFHCh <davfep_96@hotmail.com>
2016-08-24 01:19:48 -0500

    Primera tarea, terminada. Bugs parchados, la cantidad de crasheos ha disminuido.

--- src/Procesador/Controlador.java
+++ src/Procesador/Controlador.java
@@ -16,6 +16,13 @@ import javafx.scene.image.WritableImage;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Alert.AlertType;
 import javafx.application.Platform;
+import javafx.scene.canvas.Canvas;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.scene.control.Slider;
+import javafx.scene.control.Button;
 
 /**
  *
@@ -30,12 +37,27 @@ public class Controlador implements Initializable {
     @FXML
     private ImageView imagenProcesada;
 
+    @FXML
+    private Slider sliderRed;
+
+    @FXML
+    private Slider sliderGreen;
+
+    @FXML
+    private Slider sliderBlue;
+
+    @FXML
+    private Button buttonAccept;
+
     private WritableImage imagenNueva;
     private PixelReader pr;
     private PixelWriter pw;
     private int width;
     private int height;
     private Image image = null;
+    private static double rojo;
+    private static double azul;
+    private static double verde;
     
     
     @FXML
@@ -53,8 +75,8 @@ public class Controlador implements Initializable {
         File selectedFile = fileChooser.showOpenDialog(null);
         if (selectedFile != null) {
             image = new Image(new FileInputStream(selectedFile));
-            Platform.runLater(() -> imagenProcesada.setImage(image));
-            Platform.runLater(() -> imagenOriginal.setImage(image));
+            javafx.application.Platform.runLater(() -> imagenProcesada.setImage(image));
+            javafx.application.Platform.runLater(() -> imagenOriginal.setImage(image));
             pr = this.crearPR();
             if(t)//esta es la t que checo en la linea 49. para solo tener un pixelwriter.
                 pw = this.crearPW();
@@ -65,68 +87,150 @@ public class Controlador implements Initializable {
 
     //En escencia todos los metodos que siguen son lo mismo, necesito ayuda para saber que es lo que tengo que sincrinizar, si es que se requiere.
     @FXML
-    private void filtroPromedioByN(ActionEvent event) throws Exception{
+    private void filtroPromedioByN(ActionEvent event){
         try{
             FiltrosColores fc = new FiltrosColores(pw,pr,width,height,1);
-            synchronized(this){
+            synchronized(fc){
             fc.start();
             }
-            Platform.runLater(() -> imagenProcesada.setImage(imagenNueva));//Esta parecia una solucion al bug que sucede, solo disminuyo la cantidad de ocirrencias de este.
+            synchronized(this){
+            javafx.application.Platform.runLater(() -> imagenProcesada.setImage(imagenNueva));//Esta parecia una solucion al bug que sucede, solo disminuyo la cantidad de ocirrencias de este.
+            }
         }catch(Exception e){
              alerta("No hay Imagen.","Favor de abrir una imagen");
         }
     }
 
     @FXML
-    private void filtroRealByN(ActionEvent event) throws Exception{
+    private void filtroRealByN(ActionEvent event){
         try{
             FiltrosColores fc = new FiltrosColores(pw,pr,width,height,2);
-            synchronized(this){
+            synchronized(fc){
             fc.start();
             }
-            Platform.runLater(() -> imagenProcesada.setImage(imagenNueva));
+            synchronized(this){
+            javafx.application.Platform.runLater(() -> imagenProcesada.setImage(imagenNueva));
+            }
         }catch(Exception e){
              alerta("No hay Imagen.","Favor de abrir una imagen");
         }
     }
 
     @FXML
-    private void filtroRepetidoRByN(ActionEvent event) throws Exception{
+    private void filtroRepetidoRByN(ActionEvent event){
         try{
             FiltrosColores fc = new FiltrosColores(pw,pr,width,height,3);
-            synchronized(this){
+            synchronized(fc){
             fc.start();
             }
-            Platform.runLater(() -> imagenProcesada.setImage(imagenNueva));
+            synchronized(this){
+            javafx.application.Platform.runLater(() -> imagenProcesada.setImage(imagenNueva));
+            }
+
         }catch(Exception e){
              alerta("No hay Imagen.","Favor de abrir una imagen");
         }
     }
 
     @FXML
-    private void filtroRepetidoGByN(ActionEvent event) throws Exception{
+    private void filtroRepetidoGByN(ActionEvent event){
         try{
             FiltrosColores fc = new FiltrosColores(pw,pr,width,height,4);
-            synchronized(this){
+            synchronized(fc){
             fc.start();
             }
-            Platform.runLater(() -> imagenProcesada.setImage(imagenNueva));
+            synchronized(this){
+            javafx.application.Platform.runLater(() -> imagenProcesada.setImage(imagenNueva));
+            }
         }catch(Exception e){
             alerta("No hay Imagen.","Favor de abrir una imagen");
         }
     }
 
     @FXML
-    private void filtroRepetidoBByN(ActionEvent event) throws Exception{
+    private void filtroRepetidoBByN(ActionEvent event){
         try{
             FiltrosColores fc = new FiltrosColores(pw,pr,width,height,5);
-            synchronized(this){
+            synchronized(fc){
             fc.start();
             }
-            Platform.runLater(() -> imagenProcesada.setImage(imagenNueva));
+            synchronized(this){
+            javafx.application.Platform.runLater(() -> imagenProcesada.setImage(imagenNueva));
+            }
         }catch(Exception e){
             alerta("No hay Imagen.","Favor de abrir una imagen");
         }
+    }
+
+    @FXML
+    private void filtroMicaRoja(ActionEvent event){
+        try{
+            FiltrosColores fc = new FiltrosColores(pw,pr,width,height,6);
+            synchronized(fc){
+            fc.start();
+            }
+            synchronized(this){
+            javafx.application.Platform.runLater(() -> imagenProcesada.setImage(imagenNueva));
+            }
+        }catch(Exception e){
+            alerta("No hay Imagen.","Favor de abrir una imagen");
+        }
+    }
+
+     @FXML
+    private void filtroMicaVerde(ActionEvent event){
+        try{
+            FiltrosColores fc = new FiltrosColores(pw,pr,width,height,7);
+            synchronized(fc){
+            fc.start();
+            }
+            synchronized(this){
+            javafx.application.Platform.runLater(() -> imagenProcesada.setImage(imagenNueva));
+            }
+        }catch(Exception e){
+            alerta("No hay Imagen.","Favor de abrir una imagen");
+        }
+    }
+
+     @FXML
+    private void filtroMicaAzul(ActionEvent event){
+        try{
+            FiltrosColores fc = new FiltrosColores(pw,pr,width,height,8);
+            synchronized(fc){
+            fc.start();
+            }
+            synchronized(this){
+            javafx.application.Platform.runLater(() -> imagenProcesada.setImage(imagenNueva));
+            }
+        }catch(Exception e){
+            alerta("No hay Imagen.","Favor de abrir una imagen");
+        }
+    }
+
+    @FXML
+    private void filtroColoresCombinados(ActionEvent event){
+        try{
+            newWindow("micasCombinadas.fxml","Micas Combinadas");
+            System.out.println(rojo);
+            FiltrosColores fc = new FiltrosColores(pw,pr,width,height,9,rojo,verde,azul);
+            synchronized(fc){
+                fc.start();
+            }
+            synchronized(this){
+                javafx.application.Platform.runLater(() -> imagenProcesada.setImage(imagenNueva)); 
+            }
+        }catch(Exception e){
+            alerta("No hay Imagen.","Favor de abrir una imagen");
+        }
+    }
+
+    @FXML
+    private void filtroColoresCombinadosAux(ActionEvent event){
+         rojo = sliderRed.getValue();
+         verde = sliderGreen.getValue();
+         azul = sliderBlue.getValue();
+          Stage stage = (Stage)buttonAccept.getScene().getWindow();
+    stage.close();
     }
 
     private PixelWriter crearPW(){
@@ -140,6 +244,10 @@ public class Controlador implements Initializable {
         return pr;
     }
 
+    /**
+    *@param msg mensaje del header de la alerta.
+    *@param msg1 cuerpo del mensaje de alerta.
+    */
     public static void alerta(String msg,String msg1){
         Alert alert = new Alert(AlertType.ERROR);
         alert.setTitle("Error");
@@ -147,10 +255,21 @@ public class Controlador implements Initializable {
         alert.setContentText(msg1);
         alert.showAndWait();
     }
+
+    private void newWindow(String nombreArchivo, String titulo) throws Exception{
+         Parent root = FXMLLoader.load(getClass().getResource(nombreArchivo));
+            Scene scene = new Scene(root);
+            Stage stage = new Stage();
+            stage.setTitle(titulo);
+            stage.setScene(scene);
+            stage.setResizable(false);
+
+            stage.showAndWait();
+    }
     
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+
     }    
     
 }
--- src/Procesador/FiltrosColores.java
+++ src/Procesador/FiltrosColores.java
@@ -13,9 +13,13 @@ public class FiltrosColores extends Thread{
 	int width;
 	int height;
 	int operation;
+	double red; 
+	double green; 
+	double blue;
 	private double r;
 	private double g;
 	private double b; 
+
 
 	public FiltrosColores(PixelWriter pw1,PixelReader pr1,int width1, int height1, int operation1){
 		pw = pw1;
@@ -29,6 +33,20 @@ public class FiltrosColores extends Thread{
 
 	}
 
+	public FiltrosColores(PixelWriter pw1,PixelReader pr1,int width1, int height1, int operation1,double red, double green, double blue){
+		pw = pw1;
+		pr = pr1;
+		width = width1;
+		height = height1;
+		operation = operation1;
+		r = 0;
+		g = 0;
+		b = 0;
+		this.red = red;
+		this.green = green;
+		this.blue = blue;
+	}
+
 	//tarea filtro gris (R+G+B)/3
 	//filtro R*0.3    g*0.59 b*0.11
 	//filtro (R;R;R) (GGG)(BBB)
@@ -38,7 +56,7 @@ public class FiltrosColores extends Thread{
 	/**
 	* Filtro de blanco y negro, calcula el gris (R+G+B)/3.
 	*/
-	public void promByN1(){
+	private void promByN1(){
 		for(int i = 0; i < this.width; i++){
 			for(int j = 0; j < this.height; j++){
 				double prom = 0;
@@ -59,7 +77,7 @@ public class FiltrosColores extends Thread{
 	* Filtro de blanco y negro, calcula el gris multiplicando por numeros magicos,obtenidos
 	* por gente que hizo calculos especiales.
 	*/
-	public void realByN(){
+	private void realByN(){
 		for(int i = 0; i < this.width; i++){
 			for(int j = 0; j < this.height; j++){
 				double sum = 0;
@@ -80,7 +98,7 @@ public class FiltrosColores extends Thread{
 	* Estos son tres filtros si el 'color' es 1 el gris es (R,R,R), si es 2 es (G,G,G), si es 3 entonces (B,B,B).
 	* @param color un entero entre 1 y 3.
 	*/
-	public void repetidoByN(int color){
+	private void repetidoByN(int color){
 		for(int i = 0; i < this.width; i++){
 			for(int j = 0; j < this.height; j++){
 				double sum = 0;
@@ -106,6 +124,66 @@ public class FiltrosColores extends Thread{
 		}			
 	}
 
+	private void micasColor(int color){
+		for(int i = 0; i < this.width; i++){
+			for(int j = 0; j < this.height; j++){
+				double sum = 0;
+				Color c = pr.getColor(i,j);
+				Color c1 = null;
+				switch (color){
+					case 1:
+						this.r = c.getRed();
+						c1 = Color.color(r,0,0);
+						break;
+					case 2:
+						this.g = c.getGreen();
+						c1 = Color.color(0,g,0);
+						break;
+					case 3:
+						this.b = c.getBlue();
+						c1 = Color.color(0,0,b);
+						break;
+				}
+				pw.setColor(i,j,c1);
+			}
+
+		}	
+	}
+
+
+	private void micasColoresVariable(double red, double green, double blue){
+		for(int i = 0; i < this.width; i++){
+			for(int j = 0; j < this.height; j++){
+				Color c = pr.getColor(i,j);
+				Color c1 = null;
+				int red1 = 0, green1 = 0, blue1 = 0,red2 = 0, green2 = 0, blue2 = 0, red3 = 0, green3 = 0, blue3 = 0;
+				this.r = c.getRed();
+				this.g = c.getGreen();
+				this.b = c.getBlue();
+
+				red1 = (int) (r*255);
+				green1 = (int) (g*255);
+				blue1 = (int) (b*255);
+
+				red2 = (int) (red);
+				green2 = (int) (green);
+				blue2 = (int) (blue);
+
+				red3 = red1 & red2;
+				green3 = green1 & green2;
+				blue3 = blue1 & blue2;
+
+				r = ((double) red3)/255;
+				g = ((double) green3)/255;
+				b = ((double) blue3)/255;
+
+				c1 = Color.color(r,g,b);
+				pw.setColor(i,j,c1);
+			}
+
+		}	
+	}
+
 	/**
 	* Metodo para el Thread, se elige el filtro a realizar.
 	*/
@@ -126,6 +204,18 @@ public class FiltrosColores extends Thread{
 					break;
 				case 5:
 					this.repetidoByN(3);
+					break;
+				case 6:
+					this.micasColor(1);
+					break;
+				case 7:
+					this.micasColor(2);
+					break;
+				case 8:
+					this.micasColor(3);
+					break;
+				case 9: 
+					this.micasColoresVariable(red,green,blue);
 					break;
 				default:
 					System.out.println("Error");
